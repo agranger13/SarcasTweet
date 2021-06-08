60c10f36dd6c7d534dee3b87a12f08169f7ee3a0
Alexis <granger.alexis13@gmail.com>
2021-06-08 13:46:31 +0200

    Adding CORS rules and full heroku deployment

--- API_Server/app/main.py
+++ API_Server/app/main.py
@@ -1,23 +1,26 @@
 import random
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import random
-def create_app():
-    app = Flask(__name__)
 
-    @app.route('/')
-    def homepage():
-        return 'This is the homepage'
+app = Flask(__name__)
+cors = CORS(app, resources={r"/evaluate_sarcasm": {"origins": "*"}})
 
-    @app.route('/evaluate_sarcasm', methods=['POST'])
-    def evaluate_sarcasm():
-        data = request.json
-        print(data["text"])
-        result = {"score": predict(data["text"])}
-        return jsonify(result)
+@app.route('/')
+def homepage():
+    return 'This is the homepage'
 
-    def predict(text):
-        score = random.random() * 100
-        return score
+@app.route('/evaluate_sarcasm', methods=['POST'])
+@cross_origin()
+def evaluate_sarcasm():
+    print("start evaluation")
+    print(request)
+    data = request.json
+    print(data["text"])
+    result = {"score": predict(data["text"])}
+    return jsonify(result)
 
-    return app
+def predict(text):
+    score = random.random() * 100
+    return score
--- API_Server/entry_point.py
+++
@@ -1,4 +0,0 @@
-import app.main as app
-if __name__ == "__main__":
-  app_flask = app.create_app()
-  app_flask.run()

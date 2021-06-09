8ffd47a6bcedabbc0a7ccf9d7834bb2c58bb5a68
Alexis GRANGER <granger.alexis13@gmail.com>
2021-06-09 17:46:04 +0200

    Add route to push in an elasticsearch (heroku)

--- API_Server/app/main.py
+++ API_Server/app/main.py
@@ -2,10 +2,12 @@ import random
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+from elasticsearch import Elasticsearch, helpers
+import json
 import random
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/evaluate_sarcasm": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def homepage():
@@ -20,6 +22,35 @@ def evaluate_sarcasm():
     print(data["text"])
     result = {"score": predict(data["text"])}
     return jsonify(result)
+
+
+@app.route('/send_feedback', methods=['POST'])
+@cross_origin()
+def send_feedback():
+    print("sending feedback")
+    es = ES_Data()
+    data = request.json
+    print(data["text"]," => ",data["label"])
+    es.send(data["text"],data["label"])
+
+def predict(text):
+    score = random.random() * 100
+    return score
+
+class ES_Data:
+    def __init__(self):
+        self.es = Elasticsearch(
+            [
+                'https://fpi04y6zo7:k6ezgcj8zp@elm-236927195.eu-west-1.bonsaisearch.net:443'
+            ],
+            timeout=100
+        )
+
+    def send(self, text, label):
+        body = {"text": text, "label": label}
+        print(json.dumps(body))
+
+        print(self.es.index(index="feedback", body=body))
 
 def predict(text):
     score = random.random() * 100
